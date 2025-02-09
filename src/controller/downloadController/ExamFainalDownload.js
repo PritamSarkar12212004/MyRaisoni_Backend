c105963595cd4ad05505764c4bb1f057777fda2d
pritam sarkar <pritam7796419792@gmail.com>
2025-02-10 01:20:40 +0530

    remove download

--- src/controller/downloadController/ExamFainalDownload.js
+++ src/controller/downloadController/ExamFainalDownload.js
@@ -1,33 +1,6 @@
 import axios from "axios";
 
 const ExamFainalDownload = async (req, res) => {
-  const { semesterId, sessionId, token } = req.body.data;
-
-  try {
-    // Fetch PDF from external API
-    const response = await axios.get(
-      `https://ghrua.cybervidya.net/api/reports/student/grade-card/${semesterId}/${sessionId}/Regular`,
-      {
-        headers: {
-          Authorization: `GlobalEducation ${token}`,
-        },
-        responseType: "stream", // Stream the response directly
-      }
-    );
-
-    // Set headers for direct download
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader(
-      "Content-Disposition",
-      'attachment; filename="grade-card.pdf"'
-    );
-
-    // Pipe the response directly to the client
-    response.data.pipe(res);
-  } catch (error) {
-    console.error("Download Error:", error);
-    res.status(500).json({ error: "Error downloading the file" });
-  }
+  console.log("hello");
 };
-
 export default ExamFainalDownload;
