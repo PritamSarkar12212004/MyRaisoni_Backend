8bd7440b26eb563bfee72cb52174e5d09af619ab
pritam sarkar <pritam7796419792@gmail.com>
2025-02-10 00:38:00 +0530

    exam pdf downloader function implement

--- src/routes/DownloadRoute.js
+++ src/routes/DownloadRoute.js
@@ -2,7 +2,10 @@ import express from "express";
 import asyncehandler from "express-async-handler";
 import FainanceDownloadController from "../controller/downloadController/FainanceDownloadController.js";
 import ExamDownloadController from "../controller/downloadController/ExamDownloadController.js";
+import ExamFainalDownload from "../controller/downloadController/ExamFainalDownload.js";
 const router = express.Router();
 router.post("/fainanceOne", asyncehandler(FainanceDownloadController));
 router.post("/examOne", asyncehandler(ExamDownloadController));
+router.post("/exam/maindownload", asyncehandler(ExamFainalDownload));
+
 export default router;
