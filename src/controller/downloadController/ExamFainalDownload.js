77938f9f720789e1dc6e9f30b1578332af54328d
pritam sarkar <pritam7796419792@gmail.com>
2025-02-12 01:20:14 +0530

    add pdf FOR exam base 64 encoded download api

--- src/controller/downloadController/ExamFainalDownload.js
+++ src/controller/downloadController/ExamFainalDownload.js
@@ -1,6 +1,32 @@
 import axios from "axios";
 
 const ExamFainalDownload = async (req, res) => {
-  console.log("hello");
+  const { token, semesterId, sessionId } = req.body;
+  try {
+    const response = await axios.get(
+      `https://ghrua.cybervidya.net/api/reports/student/grade-card/${semesterId}/${sessionId}/Regular`,
+      {
+        headers: {
+          accept: "application/json, text/plain, */*",
+          authorization: `GlobalEducation ${token}`,
+        },
+        responseType: "arraybuffer", // Ensures binary data is received
+      }
+    );
+
+    const base64Pdf = Buffer.from(response.data).toString("base64");
+
+    // Save to file (optional)
+
+    // Send base64 data to frontend
+    res.status(200).json({
+      data: base64Pdf,
+      message: "PDF downloaded successfully",
+    });
+  } catch (error) {
+    console.error("Error downloading image:", error);
+    res.status(500).json({ error: "Failed to fetch image" });
+  }
 };
+
 export default ExamFainalDownload;
--- src/routes/DownloadRoute.js
+++ src/routes/DownloadRoute.js
@@ -3,9 +3,11 @@ import asyncehandler from "express-async-handler";
 import FainanceDownloadController from "../controller/downloadController/FainanceDownloadController.js";
 import ExamDownloadController from "../controller/downloadController/ExamDownloadController.js";
 import ExamFainalDownload from "../controller/downloadController/ExamFainalDownload.js";
+import FainanceFainalownlaod from "../controller/downloadController/FainanceFainalownlaod.js";
 const router = express.Router();
 router.post("/fainanceOne", asyncehandler(FainanceDownloadController));
 router.post("/examOne", asyncehandler(ExamDownloadController));
 router.post("/exam/maindownload", asyncehandler(ExamFainalDownload));
+router.post("/fainace/maindownload", asyncehandler(FainanceFainalownlaod));
 
 export default router;
